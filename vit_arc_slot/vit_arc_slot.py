cc2f4ebea457e7a6025d73b33d12670c012d5998
lucidrains <lucidrains@gmail.com>
2024-10-18 09:43:00 -0700

    naive slot integration

--- vit_arc_slot/vit_arc_slot.py
+++ vit_arc_slot/vit_arc_slot.py
@@ -7,7 +7,7 @@ from slot_attention import SlotAttention
 
 from x_transformers import Attention, FeedForward, RMSNorm
 
-from einops import rearrange
+from einops import rearrange, pack, unpack
 from einops.layers.torch import Rearrange
 
 # helper functions
@@ -20,6 +20,15 @@ def default(v, d):
 
 def divisible_by(num, den):
     return (num % den) == 0
+
+def pack_with_inverse(t, pattern):
+    t, packed_shape = pack(t, pattern)
+
+    def inverse(out, inverse_pattern = None):
+        inverse_pattern = default(inverse_pattern, pattern)
+        return unpack(out, packed_shape, inverse_pattern)
+
+    return t, inverse
 
 # main class
 
@@ -36,11 +45,20 @@ class SlotViTArc(Module):
         ff_mult = 4,
         attn_kwargs: dict = dict(),
         ff_kwargs: dict = dict(),
-        dropout = 0.
+        num_slots = 50,
+        slot_attn_iterations = 3,
+        dropout = 0.,
+        dim_output = None
     ):
         super().__init__()
         self.input_shape = (channels, image_size, image_size)
         assert divisible_by(image_size, patch_size)
+
+        self.slot_attn = SlotAttention(
+            num_slots = num_slots,
+            dim = dim,
+            iters = slot_attn_iterations
+        )
 
         num_patches = (image_size // patch_size) ** 2
         patch_dim = channels * patch_size ** 2
@@ -63,6 +81,8 @@ class SlotViTArc(Module):
         self.layers = layers
         self.final_norm = RMSNorm(dim)
 
+        self.to_pred = nn.Linear(dim, default(dim_output, dim))
+
     def forward(
         self,
         images
@@ -73,8 +93,16 @@ class SlotViTArc(Module):
 
         tokens = rearrange(tokens, 'b h w d -> b (h w) d')
 
+        objects = self.slot_attn(tokens)
+
+        tokens, unpack_fn = pack_with_inverse([objects, tokens], 'b * d')
+
         for attn_norm, attn, ff_norm, ff in self.layers:
             tokens = attn(attn_norm(tokens)) + tokens
             tokens = ff(ff_norm(tokens)) + tokens
 
-        return self.final_norm(tokens)
+        tokens = self.final_norm(tokens)
+
+        object_tokens, tokens = unpack_fn(tokens)
+
+        return self.to_pred(tokens)
